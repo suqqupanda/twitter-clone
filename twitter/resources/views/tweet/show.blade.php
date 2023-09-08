353362680559fd10f2cf1e48d18c7571e6a5c6c6
Nakai_Rio <2002bird2002@gmail.com>
2023-09-08 14:49:49 +0900

    ログインしているユーザーのツイートではない場合、編集・削除ボタンを見えないようにする

--- twitter/resources/views/tweet/show.blade.php
+++ twitter/resources/views/tweet/show.blade.php
@@ -6,12 +6,13 @@
     <p><strong>ユーザー名:</strong> {{ $tweet->user->name }}</p>
     <p><strong>ツイートした日付:</strong> {{ $tweet->created_at->format('Y-m-d H:i:s') }}</p>
     <p><strong>ツイート内容:</strong> {{ $tweet->tweet }}</p>
-    <a href="{{ route('tweet.edit', ['id' => $tweet->id]) }}" class="btn btn-primary">Update Tweet</a>
-    <p></p>
-    <form action="{{ route('tweet.delete', ['id' => $tweet->id]) }}" method="POST">
-        @csrf
-        @method('DELETE')
-        <button type="submit" class="btn btn-danger">Delete Tweet</button>
-    </form>
+    @if ($tweet->user->id === Auth::id())
+        <a href="{{ route('tweet.edit', ['id' => $tweet->id]) }}" class="btn btn-primary">Update Tweet</a>
+        <form action="{{ route('tweet.delete', ['id' => $tweet->id]) }}" method="POST">
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="btn btn-danger">Delete Tweet</button>
+        </form>
+    @endif
 </div>
 @endsection
